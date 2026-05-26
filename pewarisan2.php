be306bce9306cf74722a27e3dc799cb9735b7705
afrizalnd <afrizalnurd01@gmail.com>
2026-05-26 14:45:57 +0700

    first

---
+++ pewarisan2.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'pewarisan.php';
+// Strawberry is inherited from Fruit
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? <br>";
+  }
+}
+
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();
+$strawberry->intro();
+?>
